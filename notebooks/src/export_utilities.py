83d571b1dcc6bb04ae83a8bb2382fbed0920feb2
chvaldez10 <christianvaldez021@gmail.com>
2024-01-24 12:51:35 -0700

    refactored log files to json

--- notebooks/src/export_utilities.py
+++ notebooks/src/export_utilities.py
@@ -1,5 +1,6 @@
 
 from typing import List
+import json
 
 def write_filtered_filenames_to_log(file_paths: List[str], log_file: str) -> None:
     """
@@ -12,3 +13,7 @@ def write_filtered_filenames_to_log(file_paths: List[str], log_file: str) -> Non
         print(f"exported to: {log_file}")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+def export_to_json(patients: list[dict], filename: str) -> None:
+    with open(filename, "w") as json_data:
+        json.dump(patients, json_data, indent=2)
