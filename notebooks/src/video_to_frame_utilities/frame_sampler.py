28fdf825967031090a1cb8e602ab973dd8ca0f51
chvaldez10 <christianvaldez021@gmail.com>
2024-02-13 21:51:57 -0700

    create driver code for vid to png

--- notebooks/src/video_to_frame_utilities/frame_sampler.py
+++ notebooks/src/video_to_frame_utilities/frame_sampler.py
@@ -26,3 +26,12 @@ def resample_frames(old_fps: int, new_fps: int, start: int) -> List[int]:
     resampled_frame_indices = np.take(original_frame_indices, nearest_frame_indices, mode='wrap')
 
     return resampled_frame_indices.tolist()
+
+def resample_and_validate_frames(old_fps, new_fps):
+    """
+    Resamples frames based on old and new fps, and validates the frame count.
+    """
+    frames_to_pick = resample_frames(old_fps, new_fps, 1)
+    if len(frames_to_pick) != new_fps:
+        raise ValueError("Number of frames to pick is not equal to new fps")
+    return frames_to_pick
