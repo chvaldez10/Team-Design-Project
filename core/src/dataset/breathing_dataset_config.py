5a2be6213e499c56c737e690ab35978b42cd82e3
chvaldez10 <christianvaldez021@gmail.com>
2024-03-16 12:01:15 -0600

    added dataset config

---
+++ core/src/dataset/breathing_dataset_config.py
@@ -0,0 +1,10 @@
+from typing import Optional
+from torchvision.transforms import Compose
+
+class DatasetConfig:
+    def __init__(self, root_dir: str, blanket_condition: Optional[str] = None, distance: Optional[str] = None, transform: Optional[Compose] = None, max_frames: int = 100):
+        self.root_dir = root_dir
+        self.blanket_condition = blanket_condition
+        self.distance = distance
+        self.transform = transform
+        self.max_frames = max_frames
