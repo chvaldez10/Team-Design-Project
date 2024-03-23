cb3fdabfec42bb69eddb1686e1dae3e2055f7e97
chvaldez10 <christianvaldez021@gmail.com>
2024-03-22 22:49:05 -0600

    added test loop

--- core/src/deep_learning_models/three_d_cnn.py
+++ core/src/deep_learning_models/three_d_cnn.py
@@ -1,21 +1,21 @@
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torch
 class Basic3DCNN(nn.Module):
     def __init__(self):
         super(Basic3DCNN, self).__init__()
-        self.conv1 = nn.Conv3d(in_channels=3, out_channels=16, kernel_size=(3, 3, 3), stride=1, padding=1)
-        self.pool = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv3d(16, 32, kernel_size=(3, 3, 3), stride=1, padding=1)
-        # Updated size based on your input dimensions after flattening
-        self.fc1 = nn.Linear(2508800, 512)   # Adjusted to calculated size - 32 * 56* 56 * 24
-        self.fc2 = nn.Linear(512, 2)  # Assuming 2 classes
+        self.conv1 = nn.Conv3d(5, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        self.conv2 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.fc1 = nn.Linear(128 * 56 * 56, 512)
+        self.fc2 = nn.Linear(512, 1)
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        # Adjust the flattening operation to match the updated fc1 input size
-        x = x.view(-1, 2508800)  # Flatten the tensor for the fully connected layer
+        x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.dropout(x)
+        x = torch.sigmoid(self.fc2(x))
         return x
